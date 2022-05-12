168fa133a9e15255610e0c9197c09ce0d3040ffb
Anish Agarwal <86550700+agarwal-anish@users.noreply.github.com>
2022-05-12 16:43:15 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+/*Over here we are defining all the variables*/
+var first_name = ""; /*Enter your first name here in the double quotes*/
+var last_name = "";  /*Enter your last name here in the double quotes*/
+var x = ; /*Enter any number of your choice*/
+var b = ; /*Enter any number of your choice*/
+var c = ; /*Enter any number of your choice*/
+var f = ; /*Enter any number of your choice*/
+var add_name = first_name + last_name;
+/*This is the code that will be carried out in the console page*/
+console.log(add_name);
+var add_number = x + b;
+var add_number_2 = c + f;
+console.log(add_number);
+console.log(add_number_2);
